236e71241a59919f5074527c0092be22cc551a83
ajay_francis <ajay_francis@intuit.com>
2016-12-10 11:49:13 +0530

    Better responsive with bugs

--- src/uic/Head.js
+++ src/uic/Head.js
@@ -8,14 +8,16 @@ class Head extends Component {
     return (
         <div className={cardStyle.head}>
           <ProfilePicture picture={this.props.picture}/>
-          <div className={cardStyle.doctorName} >{this.props.name}</div>
-          <div className={cardStyle.speciality} >{this.props.speciality}</div>
-          <div className={cardStyle.rating} >
-            <Icon  styleClass={iconStyle.iconSmall} name="star" />
-            <Icon  styleClass={iconStyle.iconSmall} name="star" />
-            <Icon  styleClass={iconStyle.iconSmall} name="star" />
-            <Icon  styleClass={iconStyle.iconSmall} name="star" />
-            <Icon  styleClass={iconStyle.iconSmall} name="star" />
+          <div className={cardStyle.doctorDetails}>
+            <div className={cardStyle.doctorName} >{this.props.name}</div>
+            <div className={cardStyle.speciality} >{this.props.speciality}</div>
+            <div className={cardStyle.rating} >
+              <Icon  styleClass={iconStyle.iconSmall} name="star" />
+              <Icon  styleClass={iconStyle.iconSmall} name="star" />
+              <Icon  styleClass={iconStyle.iconSmall} name="star" />
+              <Icon  styleClass={iconStyle.iconSmall} name="star" />
+              <Icon  styleClass={iconStyle.iconSmall} name="star" />
+            </div>
           </div>
         </div>
     );
